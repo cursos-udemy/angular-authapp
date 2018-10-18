8dc5a94ed05feb39e50ee49c8508a623955dcbeb
gwfernandez <gwfernandez@gmail.com>
2018-10-18 09:30:34 -0300

    configuro navegacion y navbar

---
+++ src/app/app.routes.ts
@@ -0,0 +1,28 @@
+// import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './components/home/home.component';
+
+// const APP_ROUTES: Routes = [
+//     { path: 'home', component: HomeComponent },
+//     { path: '**', pathMatch:"full", redirectTo: "home"}
+// ];
+
+// export const APP_ROUTING = RouterModule.forRoot (APP_ROUTES);
+
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { PreciosComponent } from "./components/precios/precios.component";
+import { ProtegidaComponent } from "./components/protegida/protegida.component";
+
+const APP_ROUTES: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "precios", component: PreciosComponent },
+  { path: "protegida", component: ProtegidaComponent },
+  { path: "**", pathMatch: "full", redirectTo: "home" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
